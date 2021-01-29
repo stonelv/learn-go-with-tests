dc53902d2c287d2a824bd6c442a8a70fcfb96f9f
lvzhe <zlv@mycwt.com>
2021-01-29 14:54:34 +0800

    Add chapter maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,52 @@
 package maps
 
+//Dictionary used to store dic information
+type Dictionary map[string]string
+
 //Search find item from a map by key
-func Search(dictionary map[string]string, word string) string {
-	return ""
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrorNotFound
+	}
+
+	return definition, nil
+}
+
+//Add used to add the item to the dictionary
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	
+	return nil
+}
+
+//Update used to update the definition by word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+//Delete used to delete item by word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
